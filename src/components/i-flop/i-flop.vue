45a85ba4d8ea26598414c2fd280e686cbfa0def3
不爱喝橙子汁 <201410730128@stu.shmtu.edu.cn>
2021-12-15 17:33:00 +0800

    添加color-ui依赖

---
+++ src/components/i-flop/i-flop.vue
@@ -0,0 +1,20 @@
+<template>
+  <view class="curin-index" v-if="show_page">
+    <view @click="tamin(index)" v-for="(item, index) in jackPot" :key="index" class="currin" :class="[really == index + 1 ? 'animt' : '', really != index + 1 && surplus ? 'animt' : '', really == '' ? 'cinton' + (index + 1) : '', really == index + 1 && implement > 1 ? 'selected' : 'un-selected']"> {{ really == index + 1 ? flop : '' }}{{ really != index + 1 && really != '' ? biutin : '' }}{{ really == '' ? '点击翻开' : '' }} </view>
+  </view>
+</template>
+
+<script>
+export default {
+  props: {
+    jackPot: {
+      type: Array,
+      default: () => []
+    },
+    finalResult: {
+      type: Object,
+      default: () => {}
+    }
+  }
+};
+</script>
