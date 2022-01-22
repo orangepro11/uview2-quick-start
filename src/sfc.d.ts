21ab0d6cf8c7d879aac77b0c5b9eb37c76cc0c92
xuzhendong666 <810792608@Outlook.com>
2022-01-22 19:08:45 +0800

    feat: 补充类型

--- src/sfc.d.ts
+++ src/sfc.d.ts
@@ -10,6 +10,7 @@ declare module 'uview-ui' {
 
 // 1. 确保在声明补充的类型之前导入 'vue'
 import Vue from 'vue';
+import { bus } from './helpers/bus'
 
 // 2. 定制一个文件，设置你想要补充的类型
 //    在 types/vue.d.ts 里 Vue 有构造函数类型
@@ -17,7 +18,7 @@ declare module 'vue/types/vue' {
   // 3. 声明为 Vue 补充的东西
   interface Vue {
     $u: any;
-    $bus: InstanceType<typeof Vue>;
+    $bus: typeof bus;
     mescrollInit: any;
     $eventbus: Record<string, Function>;
     $Trigger: (name: string, ...args: any[]) => void;
